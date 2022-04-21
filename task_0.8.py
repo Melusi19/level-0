127bbcfe07a13009a7cff40ae237a608c18f6160
Melusi19 <69846426+Melusi19@users.noreply.github.com>
2022-04-21 09:17:04 +0200

    Update task_0.8.py

--- task_0.8.py
+++ task_0.8.py
@@ -2,8 +2,7 @@ def convert():
 
     minutes = 350
     hours = 60
-
-    print(minutes//hours)
-    print(minutes%hours)
+    
+    print(minutes//hours, "hours", minutes%hours, "minutes")
 
 convert()
