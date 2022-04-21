306376ce4329dd184201486e66643ad7f8279db0
Melusi19 <69846426+Melusi19@users.noreply.github.com>
2022-04-21 21:00:41 +0200

    Update task_0.8.py

--- task_0.8.py
+++ task_0.8.py
@@ -1,8 +1,10 @@
-def convert():
-
-    minutes = 350
-    hours = 60
+def convert(time):
     
-    print(minutes//hours, "hours", minutes%hours, "minutes")
-
-convert()
+    hour = time // 3600
+    minutes = time // 60
+    time %= 60
+    
+    return "%02d hours %02d minutes" % (minutes, time)
+      
+n = 133
+print(convert(n))
