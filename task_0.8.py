69914e11bb871702441018cfe84d432c34cb94b0
Melusi19 <69846426+Melusi19@users.noreply.github.com>
2022-04-22 10:41:59 +0200

    Update task_0.8.py

--- task_0.8.py
+++ task_0.8.py
@@ -4,7 +4,7 @@ def convert(time):
     minutes = time // 60
     time %= 60
     
-    return "%02d hours %02d minutes" % (minutes, time)
+    return "%02d hours %02d minutes " % (minutes, time)
       
 n = 133
 print(convert(n))
