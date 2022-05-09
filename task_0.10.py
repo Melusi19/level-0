cb9e9fff51827a6e230c5caee6d5a917e1806168
Melusi19 <69846426+Melusi19@users.noreply.github.com>
2022-05-09 21:38:38 +0200

    Delete task_0.10.py

--- task_0.10.py
+++
@@ -1,8 +0,0 @@
-def common_characters(str1, str2):
-    ch1 = set(str1)
-    ch2 = set(str2)
-
-    intersection = ch1.intersection(ch2)
-    common = ", ".join(intersection)
-
-    print(f"Common letters: {common}")
