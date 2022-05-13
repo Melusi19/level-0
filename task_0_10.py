f490c6c076417c8f293c89329271b831f27a823e
Melusi19 <69846426+Melusi19@users.noreply.github.com>
2022-05-13 20:24:03 +0200

    Update task_0_10.py

--- task_0_10.py
+++ task_0_10.py
@@ -1,7 +1,7 @@
 def common_letters(str1, str2):
     common = ""
-    for letter in str1:
-        if letter in str2:
+    for letter in str1.lower():
+        if letter in str2.lower():
             common += letter 
     return "Common letters: " + ','.join(common)
 
