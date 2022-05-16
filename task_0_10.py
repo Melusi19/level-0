82060a2b4b45f35153883c2398e9eff078c10d13
Melusi19 <69846426+Melusi19@users.noreply.github.com>
2022-05-16 13:46:58 +0200

    Update task_0_10.py

--- task_0_10.py
+++ task_0_10.py
@@ -3,6 +3,6 @@ def common_letters(str1, str2):
     for letter in str1.lower():
         if letter in str2.lower():
             common += letter 
-    return "Common letters: " + ','.join(common)
+    return "Common letters: " + ', '.join(common)
 
 print(common_letters("house","computers"))
