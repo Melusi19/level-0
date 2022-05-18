4146d0fbd53dd65b07ae8f3c47cdbfe8dc929193
Melusi19 <69846426+Melusi19@users.noreply.github.com>
2022-05-18 10:11:53 +0200

    Update task_0_9.py

--- task_0_9.py
+++ task_0_9.py
@@ -2,11 +2,10 @@ def print_vowels(str):
     vowels = "aeiouAEIOU"
     result = list()
     for letter in str: 
-        l = letter.lower()
-        if ( l in vowels ) and (l not in result ):
-            result.append(l)
+        new_string  = letter.lower()
+        if ( new_string  in vowels ) and (new_string  not in result ):
+            result.append(new_string )
     print ("Vowels: ", end = '')
-    #Sep is a parameter in python that primarily formats the printed statements
     print(*result,sep=', ') 
     
 print_vowels("HEello")
